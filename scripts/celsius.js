459e3fbcb4a67fcec4d569890386f7838e8f1db5
Jason Fordyce <jasonfordyce@Jasons-MacBook-Air.local>
2024-06-24 18:10:42 -0700

    updated C to F conversion

--- scripts/celsius.js
+++ scripts/celsius.js
@@ -1,24 +1,12 @@
-function convert(){
-    let num1 = prompt("Enter Temperature in Celsius:");
-
-
-    /*let temperature = "celsius";
-    let operation = prompt("Select the operation: 1. Addition, 2. Subtraction, 3. Multiplication, 4.Division");
-
-    if(operation==1){
-        //addition operation
-        let result = Number(num1) + Number(num2);
-        //display result on HTML
-        document.getElementById("results").innerHTML=`<p>The conversion for celsius is: ${result}</p>`;
-    }
-}*/
-
-function convert(celsius) {
-    console.log('Value in celsius:', celsius +"°C");
-    let fahrenheit = celsius * 9 / 5 + 32;
-    console.log('Value in Fahrenheit:', fahrenheit +"°F");
-    return fahrenheit;
-    document.getElementById("results").innerHTML=`<p>The conversion for celsius is: ${result}</p>`;
+function convert() {
+    let celsius = prompt("Enter Temperature in Celsius:");
+    console.log('Value in Celsius:', celsius +"°C");
+    let results = celsius * 9 / 5 + 32;
+    console.log('Value in Fahrenheit:', results +"°F");
+    document.getElementById("results").innerHTML=`<p> ${results} The conversion for celsius is: </p>`;
+    
 }
-convert(50);
-}
+
+
+
+
